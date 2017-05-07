6c226d46338ba1ff0ec879dbd0bb6a50ed63cfb7
Erich Kaderka <erich.kaderka@gmail.com>
2017-05-07 20:31:36 +0200

    Init

---
+++ lib/foodies.ex
@@ -0,0 +1,18 @@
+defmodule Foodies do
+  @moduledoc """
+  Documentation for Foodies.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Foodies.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/foodies_test.exs
@@ -0,0 +1,8 @@
+defmodule FoodiesTest do
+  use ExUnit.Case
+  doctest Foodies
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
