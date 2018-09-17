0f0bc820851fa84bf7a01b23dbb445fc4ed7e3d6
Matej Minarik <mato.minarik@azet.sk>
2018-09-17 20:19:25 +0200

    Use Cowboy2

--- lib/foodies/router.ex
+++ lib/foodies/router.ex
@@ -6,7 +6,7 @@ defmodule Foodies.Router do
 
   def start_link(port) do
     {port_as_integer, _} = Integer.parse(port)
-    Plug.Adapters.Cowboy.http(Foodies.Router, [], port: port_as_integer)
+    Plug.Adapters.Cowboy2.http(Foodies.Router, [], port: port_as_integer)
   end
 
   get "/" do
